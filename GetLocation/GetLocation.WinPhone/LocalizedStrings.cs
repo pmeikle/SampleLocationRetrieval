d8c2581a91fe760b67200ae9599a701c25e4329b
Pierce Meikle <pierce.meikle@aciwebs.com>
2015-09-10 12:46:10 -0400

    Create project

---
+++ GetLocation/GetLocation.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using GetLocation.WinPhone.Resources;
+
+namespace GetLocation.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
